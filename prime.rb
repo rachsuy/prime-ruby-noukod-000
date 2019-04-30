545cc7003b72d4482853b9aea04398127c111742
Rachele Suy <rachelesuy@gmail.com>
2019-04-30 18:54:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
+require 'math'
 # Add  code here!
+
+def prime?(number)
+  start_number = 2
+  if number < 1
+    return false 
+  elsif
+  for i in start_number..number
+    
+  end
+  number ==start_number 
+  
+    true
+   else
+        (start_number..number/start_number).none? { |i| number % i == 0}
+    end
+
+ 
+  
+end
