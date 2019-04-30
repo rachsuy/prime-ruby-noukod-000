26722c306413300fb39754167118e284394d3f86
Rachele Suy <rachelesuy@gmail.com>
2019-04-30 18:58:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+
+
+def prime?(number)
+  isPrime = true
+  start_number = 2
+  if number <= 1
+    isPrime = false 
+  elsif
+  for i in start_number..number - 1
+    if number % i == 0
+        isPrime = false
+    end
+  end
+  end
+      isPrime
+end
